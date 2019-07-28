3cf854bbfb2f8349f6142f5d24c55db03413411c
Olya.Nikulina <nickulina_olya@mail.ru>
2019-07-28 10:58:33 +0300

    add form for appeal

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 import { Collapse, Jumbotron, Container, NavItem, Button } from 'reactstrap';
 
