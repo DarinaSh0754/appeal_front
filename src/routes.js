d62b91c6070df04939010de2ce8ac098a9ff2706
Olya.Nikulina <nickulina_olya@mail.ru>
2019-07-28 05:34:32 +0300

    фикс навигации в header

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
-import Layout from './components/layout/layout';
 import Home from './pages/home';
+import Login from './pages/login';
+import Registration from './pages/registration';
 
 const routes = [
     { path: '/', exact: true, name: 'Главная' },
-    { path: '/home', exact: true, name: 'Домашняя страница', component: Home }    
+    { path: '/home', exact: true, name: 'Домашняя страница', component: Home },
+    { path: '/login', exact: true, name: 'Вход', component: Login },
+    { path: '/registration', exact: true, name: 'Регистрация', component: Registration }   
   ];
   
   export default routes;
