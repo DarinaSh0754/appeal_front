1f6654c42c896f03897bdce694d895cf8dcf22f6
Olya.Nikulina <nickulina_olya@mail.ru>
2019-07-28 04:18:44 +0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,8 @@ import Layout from './components/layout/layout';
 import Home from './pages/home';
 
 const routes = [
-    { path: '/', exact: true, name: 'Главная', component: Layout },
-    { path: '/home', exact: true, name: 'Домашняя страница', component: Home }
-    
+    { path: '/', exact: true, name: 'Главная' },
+    { path: '/home', exact: true, name: 'Домашняя страница', component: Home }    
   ];
   
   export default routes;
