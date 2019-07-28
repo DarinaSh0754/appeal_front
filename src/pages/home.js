473b371386048c1525f1d49fbec93872f2720e15
Vladimir <vvkorotaev@inbox.ru>
2019-07-28 09:32:22 +0300

    fix reg

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import './home.css';
-import {
-  Collapse,
-  Jumbotron,
-  Container,
-  NavItem,
-  Button
-} from 'reactstrap';
+import { Collapse, Jumbotron, Container, NavItem, Button } from 'reactstrap';
+import { Redirect, Route, Switch, Link } from 'react-router-dom';
 
 export default class Home extends Component {
   constructor(props) {
@@ -14,18 +9,19 @@ export default class Home extends Component {
   }
 
   render() {
-
-    return (  
+    return (
       <div className="vedro">
         <div className="container">
-       <Button color="link">
-          <Link to="/appeal" className="link">
-            Создать обращение
-          </Link>
-        </Button>
-          <h1 >Fluid jumbotron</h1>
-          <p >This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-
+          <Button color="link">
+            <Link to="/appeal" className="link">
+              Создать обращение
+            </Link>
+          </Button>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
         </div>
       </div>
     );
